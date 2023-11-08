5c4751a6b70656abba133c6c5865ef2d1256857a
Ricardo Pérez <ricpelo@gmail.com>
2023-11-08 19:26:41 +0100

    Crea tabla artículos

---
+++ database/migrations/2023_11_08_182010_create_articulos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->id();
+            $table->string('denominacion');
+            $table->decimal('precio', 6, 2);
+            $table->foreignId('categoria_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articulos');
+    }
+};
