3426922930d2dae8ab9df648b58fbc8453457f2b
Ricardo Pérez <ricpelo@gmail.com>
2023-11-14 17:41:26 +0100

    Realizado index de Categorías

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -1,5 +1,31 @@
 <x-guest-layout>
-    @foreach ($categorias as $categoria)
-        {{ $categoria->nombre }}
-    @endforeach
+    <div class="relative overflow-x-auto w-3/4 mx-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Categoría
+                    </th>
+                    <th scope="col" class="px-6 py-3" colspan="2">
+                        Acción
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categorias as $categoria)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $categoria->nombre }}
+                        </th>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Borrar</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </x-guest-layout>
