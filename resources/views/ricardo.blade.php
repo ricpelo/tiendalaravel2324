3ae8e509f2f36855589e01d12cfe425a99ff2c37
Ricardo Pérez <ricpelo@gmail.com>
2023-11-23 19:49:41 +0100

    Cambia todos los guest por app

--- resources/views/ricardo.blade.php
+++
@@ -1,5 +0,0 @@
-<x-guest-layout>
-    Hola
-    <x-danger-button>Hola</x-danger-button>
-    <x-text-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')" required autofocus autocomplete="username" />
-</x-guest-layout>
